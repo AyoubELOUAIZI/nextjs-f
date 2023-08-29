99ad1e00602ab8d2f3db8415d7a07a90b6781728
AyoubELOUAIZI <ayoubelouaizi@gmail.com>
2023-08-29 19:49:14 +0100

    first commit

---
+++ src/app/prediction/[name]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return <div>
+        {/* this is going to be displayed only if the internet is a bit slow and it will be gone very quickly */}
+      loading page ...
+    </div>;
+  }
+  
---
+++ src/app/start-a/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <p>     nice I toled you the name should not start with 'A'
+</p>
+    </div>
+  );
+};
+
+export default About;
